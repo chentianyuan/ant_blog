720aa33decbbc9f5f99b6ff42ee8f7dbdf39da9a
chenty1 <chenty1@guahao.com>
2019-07-07 21:48:43 +0800

    1核1G的腾讯云的机器一构建就死机`

---
+++ build/precache-manifest.541a0dcfadcbd08e2af1ae4d9e2e0b30.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4d7db08b269e68a63d6e177c4ef1d314",
+    "url": "/index.html"
+  },
+  {
+    "revision": "930b7e29040cee5e2125",
+    "url": "/static/css/2.b92aed00.chunk.css"
+  },
+  {
+    "revision": "bbf333053371d950a063",
+    "url": "/static/css/main.c281a302.chunk.css"
+  },
+  {
+    "revision": "930b7e29040cee5e2125",
+    "url": "/static/js/2.e4f5e4f6.chunk.js"
+  },
+  {
+    "revision": "bbf333053371d950a063",
+    "url": "/static/js/main.06e1e54b.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "a8ab34e340b418ffb0aa5f796b9971e4",
+    "url": "/static/media/avatar.a8ab34e3.png"
+  },
+  {
+    "revision": "a141b87e6b0faafa082d9a65a791956f",
+    "url": "/static/media/sentry-pattern.a141b87e.png"
+  }
+]);
--- ecosystem.config.js
+++ ecosystem.config.js
@@ -24,7 +24,7 @@ module.exports = {
       ref  : 'origin/master',
       repo : 'https://github.com/chentianyuan/ant_blog.git',
       path : '/usr/project-manger',
-      'post-deploy' : 'npm install && npm run build && pm2 reload ecosystem.config.js --env production'
+      'post-deploy' : 'pm2 reload ecosystem.config.js --env production'
     }
   }
 };
